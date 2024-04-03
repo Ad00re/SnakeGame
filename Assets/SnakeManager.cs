4771933249520a29d25e357323f70de0f7b42e58
Christina Wang <christinawang521@gmail.com>
2024-04-02 23:38:21 -0400

    add check for collision to wall/obstacles/itself, add replay, add food location generation check

--- Assets/SnakeManager.cs
+++ Assets/SnakeManager.cs
@@ -12,19 +12,18 @@ public class SnakeManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public float speed = 1f;
-
     
-    [SerializeField] public Vector2 food;
-    public List<Vector2> snake;
+    public Vector2Int food;
+    public List<Vector2Int> snake;
+    public List<Vector2Int> obstacles;
     
-    public Vector3 headPosition;
+    
     public Vector3 foodPosition;
     public List<GameObject> snakeDisplay;
+
+    public Vector2Int direction = new Vector2Int(1, 0);
+    public Vector2Int tempDirection = new Vector2Int(1,0);
     
-    
-    
-    public Vector2 direction = Vector2.right;
-    public Vector2 tempDirection = Vector2.right;
     
     
     public float time;
@@ -40,44 +39,44 @@ public class SnakeManager : MonoBehaviour
     void Start()
     {
         time = 0f;
+        food = new Vector2Int(2, 0);
+        snake = new List<Vector2Int> { new Vector2Int(0, 0) };
+        snakeDisplay = new List<GameObject> {head};
+        //create obstacles
+        obstacles = new List<Vector2Int>();
+        int[] values = { -1, 1 };
+        foreach (int i in values)
+        {
+            foreach (int j in values)
+            {
+                obstacles.Add(new Vector2Int(7 * i, 7 * j));
+                obstacles.Add(new Vector2Int(7 * i, 6 * j));
+                obstacles.Add(new Vector2Int(6 * i, 7 * j));
+            }
+        }
         
-        food = new Vector2(2, 0);
-        snake = new List<Vector2>();
-        snake.Add(new Vector2(0, 0));
-        snakeDisplay = new List<GameObject>();
-        snakeDisplay.Add(head);
-        
-        foodPosition = 0.5f*new Vector3(food.x, food.y, 0);
-        foodObject.transform.position = foodPosition;
     }
 
     // Update is called once per frame
     void Update()
     {
-        for(int i = 0;i<snake.Count;i++)
-        {
-            snakeDisplay[i].gameObject.transform.position = 0.5f * new Vector3(snake[i].x, snake[i].y, 0);
-        }
-        
         time += Time.deltaTime;
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            tempDirection = Vector2.up;
+            tempDirection = new Vector2Int(0, 1);
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            tempDirection = Vector2.down;
+            tempDirection = new Vector2Int(0, -1);
         }
         else if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            tempDirection = Vector2.left;
+            tempDirection = new Vector2Int(-1, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            tempDirection = Vector2.right;
+            tempDirection = new Vector2Int(1, 0);
         }
-        foodPosition = 0.5f*new Vector3(food.x, food.y, 0);
-        foodObject.transform.position = foodPosition;
         
         if (time*speed> 1)
         {
@@ -85,55 +84,69 @@ public class SnakeManager : MonoBehaviour
             direction = tempDirection;
             snake.Insert(0, snake[0] + direction);
             snake.RemoveAt(snake.Count-1);
-            
-           
-            
+        }
+        //view
+        foodPosition = 0.5f*new Vector3(food.x, food.y, 0);
+        foodObject.transform.position = foodPosition;
+        //head move depend on direction 
+        Vector2 location =0.5f * Vector2.Lerp(snake[0], snake[0]+direction, time * speed);
+        snakeDisplay[0].gameObject.transform.position = new Vector3(location.x, location.y, 0);
+        // body move depend on previous body
+        for(int i = 1;i<snake.Count;i++)
+        {
+            location = 0.5f * Vector2.Lerp(snake[i], snake[i - 1], time * speed);
+            snakeDisplay[i].gameObject.transform.position = new Vector3(location.x, location.y, 0);
         }
         //check for eat food
         if (snake[0] + direction == food)
         {
             // //create a new body
-            snake.Add(food);
+            snake.Insert(0,food);
             snakeDisplay.Add(Instantiate(bodyPrefab));
+            score += 1; 
+            scoreText.text = "Score: " + score;
+            do
+            { 
+                food = new Vector2Int(Random.Range(-8, 8), Random.Range(-8, 8));
+            } while (obstacles.Contains(food) || snake.Contains(food));
             
-            food = new Vector2(Random.Range(-10, 10), Random.Range(-10, 10));
             
-            
+        }
+        // check for collide in to obstacle
+        foreach(Vector2 obstacle in obstacles)
+        {
+            if (snake[0] + direction == obstacle)
+            {
+                Die();
+            }
+        }
+        // check for collide to wall
+        if (Math.Abs((snake[0] + direction).x)==9 || Math.Abs((snake[0] + direction).y)==9)
+        {
+            Die();
+        }
+        // check for collide to it self
+        for(int i = 1;i<snake.Count;i++)
+        {
+            if (snake[0] + direction == snake[i])
+            {
+                Die();
+            }
         }
     }
-    
 
-    private void OnCollisionEnter2D(Collision2D other)
+
+    void Die()
     {
-        Debug.Log("hit something");
-        //debug for case when touching
-        if (other.gameObject.tag == "food")
-        {
-            // Debug.Log("eat food");
-            // GameObject newBody = Instantiate(body);
-            // newBody.transform.position = food.transform.position;
-            //
-            // int x = Random.Range(0, 12);
-            // int y = Random.Range(0, 12);
-            // food.transform.position = new Vector3(0.5f * x - 3, 0.5f * y - 3, 0);
-            // score += 1; 
-            // scoreText.text = "Score: " + score.ToString();
-            // //add a body to the previous food location
-            
-            
-        }
-        else if (other.gameObject.tag == "obstacle")
-        {
-            Debug.Log("hit obstacle");
-            Time.timeScale = 0f;
-            replayButton.SetActive(true);
-        }
+        Time.timeScale = 0f;
+        replayButton.SetActive(true);
     }
 
     public void Replay()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene("SampleScene");
+        Start();
     }
     
 }
